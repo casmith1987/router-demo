0c18dafe3cf81a21423cf31d4965e58358b813d8
casmith1987 <c.a.smith1987@gmail.com>
2020-10-14 09:59:56 -0500

    First router demo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Route, Switch, NavLink } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import NotFound from './NotFound'
 
 class Main extends React.Component {
   constructor() {
@@ -10,7 +14,18 @@ class Main extends React.Component {
   render() {
     return (
       <div id='app'>
-        <main>No Routes yet</main>
+        <nav>
+          <NavLink to='/' activeClassName='selected'>Home Page</NavLink>
+          <NavLink to='/about' activeClassName='selected'>About Page</NavLink>
+          <NavLink to='/asdf' activeClassName='selected'>404</NavLink>
+        </nav>
+        <main>
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/about' component={About}/>
+            <Route path='/' exact component={NotFound}/>
+          </Switch>
+        </main>
       </div>
     )
   }
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home(props) {
+  return (
+    <div>
+      <h1>This is the Not Found</h1>
+      <p>No Match</p>
+    </div>
+  )
+}
+
+export default Home
